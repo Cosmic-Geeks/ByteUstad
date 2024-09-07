35023405c2d7ef0a5940dae9dd47ec987ab0b765
mameru-carr <samuelcarr.official@gmail.com>
2024-09-07 13:05:15 +0530

    Initialize Vue.js, Pug, Sass and TailwindCSS

---
+++ README.md
@@ -0,0 +1,3 @@
+# ByteUstad
+
+A blog by [Akshara A](https://github.com/akshara-a) and [Mameru Carr](https://github.com/mameru-carr)
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'], // Remove unused styles in production
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+    content: [],
+  plugins: [],
+}
