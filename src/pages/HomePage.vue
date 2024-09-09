17de6c10dcd5370d5cc0a3107981e1c79ba524b9
mameru-carr <samuelcarr.official@gmail.com>
2024-09-09 15:13:23 +0530

    Restore Front Page text, and add some typography

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template lang="pug">
+.flex.justify-center.items-center.min-h-screen
+    #welcome
+        h1.text-center.text-7xl.font-title.my-4
+            | Welcome To ByteUstad
+        p.text-center.text-4xl
+            | A blog by #[a(href="#") Akshara A] and #[a(href="#") Mameru Carr]
+</template>
+
+<style scoped lang="sass">
+#welcome
+    p
+        font-family: Alegreya, serif
+    a
+        text-decoration: underline
+        font-family: Courgette, serif
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'], // Remove unused styles in production
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'title': ["Oswald", "sans-serif"],
+    },
     extend: {},
   },
   variants: {
