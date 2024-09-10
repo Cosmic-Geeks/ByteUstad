6d00bbc5eb355e8f6d262a030b55f63d3f16f01e
mameru-carr <samuelcarr.official@gmail.com>
2024-09-10 16:29:54 +0530

    Add Nav Bar on Front Page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -4,17 +4,34 @@
 
 <template lang="pug">
 .flex.justify-center.items-center.min-h-screen
-    #welcome
-        h1.text-center.text-7xl.font-title.my-4
+    #welcome.flex.flex-col.space-y-4
+        h1.text-center.text-7xl.font-title
             | Welcome To ByteUstad
         p.text-center.text-4xl
-            | A blog by #[a(href="#") Akshara A] and #[a(href="#") Mameru Carr]
+            | A blog by #[a(href="#" class="author") Akshara A] and #[a(href="#" class="author") Mameru Carr]
+        nav.text-2xl
+            ul.flex.justify-between.text-2xl
+                li
+                    a(href="#")
+                        | Categories
+                li
+                    a(href="#")
+                        | Articles
+                li
+                    a(href="#")
+                        | About 
 </template>
 
 <style scoped lang="sass">
 #welcome
     p
-        font-family: Alegreya, serif
-    a
+        font-family: "Alegreya", serif
+    nav
+        ul
+            li
+                a
+                    font-family: "Josefin Sans", serif
+                    text-transform: uppercase
+    .author
         font-family: Courgette, serif
 </style>
