d7e4d820c4f9d0c55f69914630d9fae3b942eeac
mameru-carr <samuelcarr.official@gmail.com>
2024-09-21 01:55:21 +0530

    Major Typography Update: Use Recursive Font

---
+++ src/components/NavComponent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import router from "@/router";
+const navItems = ['Articles', 'Snippets', 'About']
+</script>
+
+<template lang="pug">
+nav.flex.items-center.text-2xl
+    ul.flex.gap-8
+        li(v-for="navItem in navItems" :key="navItem")
+            | #[a(@click='router.push({ name: navItem })') {{ navItem }}]
+</template>
+
+<style scoped lang="sass">
+li
+    font-variation-settings: "MONO" 1, "CASL" 0, "wght" 400, "slnt" 0, "CRSV" 0.5
+    a
+        display: block
+</style>
