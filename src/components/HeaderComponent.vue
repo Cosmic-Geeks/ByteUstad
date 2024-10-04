1c8918480d9d87085c3b541b92ac1c36062ab9f5
mameru-carr <samuelcarr.official@gmail.com>
2024-10-04 21:49:51 +0530

    Move Log and Nav to Edges

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -6,18 +6,15 @@ import MainContainer from "@/components/Layout/MainContainer.vue";
 
 <template lang="pug">
 header
-    main-container
-        .header.flex.justify-between.flex-wrap
-                p.text-3xl.font-title(@click="router.push({name: 'Home'})")
-                    | ByteUstad
-                nav-component
+    .flex.flex-row.justify-between.flex-wrap.m-4.p-4
+        p.text-3xl.font-title(@click="router.push({name: 'Home'})")
+            | ByteUstad
+        nav-component
 </template>
 
 <style lang="sass" scoped>
 header
     border-bottom: 2px solid firebrick
-    .header
-        width: 60%
     p
         margin-left: 0.5rem
         font-variation-settings: "MONO" 0, "CASL" 0.5, "wght" 500, "slnt" 0, "CRSV" 0.5
